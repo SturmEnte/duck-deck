4675a71377e51e2ce6598870c1e402601e4444d5
SturmEnte <sturmente@proton.me>
2022-08-16 20:37:58 +0200

    Test id interpretation

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,15 +1,23 @@
 use std::{net::TcpListener, io::Read};
+use enigo::{Enigo, Key, KeyboardControllable};
 
 fn main() {
+    let mut enigo = Enigo::new();
     let listener = TcpListener::bind("localhost:3030").unwrap();
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         
         let mut buffer = [0; 34];
-        stream.read(&mut buffer).unwrap();
-        println!("{:?}", String::from_utf8_lossy(&buffer[..]));
-        let id = String::from_utf8_lossy(&buffer[..]);
-        println!("Id: {}", id)
+        let size = stream.read(&mut buffer).unwrap();
+        let id = String::from_utf8_lossy(&buffer[..size]);
+        println!("Id: {}", id);
+
+        if id == "1" {
+            enigo.key_click(Key::Layout('^'));
+            println!("Key click");
+        } else {
+            println!("Not 1");
+        }
     }
 }
