16eb1fa5a7425271aacb50ab5084b93b62399043
SturmEnte <sturmente@proton.me>
2022-11-19 22:40:48 +0100

    Config loading

--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -1,5 +1,6 @@
 extern crate serde_json;
 
+use std::collections::HashMap;
 use std::{net::TcpListener, io::Read};
 use enigo::{Enigo, Key, KeyboardControllable};
 use serde_json::Value as JsonValue;
@@ -17,7 +18,29 @@ fn main() {
         Err(error) => panic!("{}", error),
     };
 
-    println!("{}", config);
+    let mut actions: HashMap<String, Action> = HashMap::new();
+    
+    // The program will crash, when the first layer of the config isn't an array
+    let conifg_array = config.as_array().unwrap();
+    conifg_array.iter().for_each(|obj| {
+        let id = obj["id"].as_str().unwrap().to_string();
+        let action_type = obj["action"]["type"].as_str().unwrap();
+
+        match action_type {
+            "key-press" => {
+                let key = obj["action"]["key"].as_str().unwrap().to_string();
+                let alt = obj["action"]["alt"].as_bool().unwrap();
+                let ctrl = obj["action"]["ctrl"].as_bool().unwrap();
+                let shift = obj["action"]["shift"].as_bool().unwrap();
+
+                actions.insert(id, {Action { key: key, alt: alt, ctrl: ctrl, shift: shift }});
+            },
+            _ => {
+                panic!("Unknown action type");
+            }
+        }
+
+    });
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
@@ -53,4 +76,11 @@ fn load_config() -> Result<JsonValue, String> {
     } else {
         return Err(res.unwrap_err().to_string());
     }
+}
+
+struct Action {
+    key: String,
+    alt: bool,
+    ctrl: bool,
+    shift: bool
 }
